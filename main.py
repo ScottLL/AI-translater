71ce5e9ba4e6fbb1fca4b8e4708f69eda1a723b9
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2023-03-19 21:41:58 -0400

    modify main.py

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-# from googletrans import Translator, LANGUAGES
-
-# from langdetect import detect
-# import datetime
-
-# def translate(text: str, input_language: str, target_language: str) -> str:
-#     # Check if target language is supported by Google Translate
-#     # if target_language not in LANGUAGES.keys():
-#     #     return "Unsupported language"
-
-#     if input_language not in LANGUAGES.keys():
-#         input_language = detect(text)
-
-#     if input_language == target_language:
-#         return text
-
-#     translator = Translator()
-#     translation = translator.translate(text, src=input_language, dest=target_language)
-
-#     if translation:
-#         return translation.text
-#     else:
-#         return "Translation failed"
-
-# def save_conversation(conversation_history):
-#     filename = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
-#     with open(filename, "w") as f:
-#         for original_text, translated_text in conversation_history:
-#             f.write("Original Text:\n" + original_text + "\n")
-#             f.write("Translated Text:\n" + translated_text + "\n")
-#             f.write("\n")
 from googletrans import Translator, LANGUAGES
 
 import datetime
